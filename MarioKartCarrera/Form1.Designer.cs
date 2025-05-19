7969787eb9b37a9260e1aded46aed6bf529fbbb0
lalaj <lalaj@EDUARDO>
2025-05-18 18:23:11 -0700

    Agregar archivos de proyecto.

---
+++ MarioKartCarrera/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MarioKartCarrera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.C2 = new System.Windows.Forms.PictureBox();
+            this.C4 = new System.Windows.Forms.PictureBox();
+            this.C1 = new System.Windows.Forms.PictureBox();
+            this.C3 = new System.Windows.Forms.PictureBox();
+            this.C5 = new System.Windows.Forms.PictureBox();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnPausa = new System.Windows.Forms.Button();
+            this.btnReinicio = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // C2
+            // 
+            this.C2.Image = global::MarioKartCarrera.Properties.Resources.c2;
+            this.C2.Location = new System.Drawing.Point(62, 285);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(44, 40);
+            this.C2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C2.TabIndex = 1;
+            this.C2.TabStop = false;
+            // 
+            // C4
+            // 
+            this.C4.Image = global::MarioKartCarrera.Properties.Resources.c4;
+            this.C4.Location = new System.Drawing.Point(62, 331);
+            this.C4.Name = "C4";
+            this.C4.Size = new System.Drawing.Size(44, 40);
+            this.C4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C4.TabIndex = 2;
+            this.C4.TabStop = false;
+            // 
+            // C1
+            // 
+            this.C1.Image = global::MarioKartCarrera.Properties.Resources.C1;
+            this.C1.Location = new System.Drawing.Point(34, 285);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(44, 40);
+            this.C1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C1.TabIndex = 3;
+            this.C1.TabStop = false;
+            // 
+            // C3
+            // 
+            this.C3.Image = global::MarioKartCarrera.Properties.Resources.c3;
+            this.C3.Location = new System.Drawing.Point(62, 373);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(44, 40);
+            this.C3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C3.TabIndex = 4;
+            this.C3.TabStop = false;
+            // 
+            // C5
+            // 
+            this.C5.Image = global::MarioKartCarrera.Properties.Resources.c5;
+            this.C5.Location = new System.Drawing.Point(34, 360);
+            this.C5.Name = "C5";
+            this.C5.Size = new System.Drawing.Size(44, 40);
+            this.C5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C5.TabIndex = 5;
+            this.C5.TabStop = false;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.BackColor = System.Drawing.Color.Transparent;
+            this.btnInicio.BackgroundImage = global::MarioKartCarrera.Properties.Resources.btninicio;
+            this.btnInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnInicio.Location = new System.Drawing.Point(419, 12);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(53, 52);
+            this.btnInicio.TabIndex = 6;
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnPausa
+            // 
+            this.btnPausa.BackColor = System.Drawing.Color.Transparent;
+            this.btnPausa.BackgroundImage = global::MarioKartCarrera.Properties.Resources.btnpausa;
+            this.btnPausa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPausa.Location = new System.Drawing.Point(507, 12);
+            this.btnPausa.Name = "btnPausa";
+            this.btnPausa.Size = new System.Drawing.Size(53, 52);
+            this.btnPausa.TabIndex = 7;
+            this.btnPausa.UseVisualStyleBackColor = false;
+            this.btnPausa.Click += new System.EventHandler(this.btnPausa_Click);
+            // 
+            // btnReinicio
+            // 
+            this.btnReinicio.BackColor = System.Drawing.Color.Transparent;
+            this.btnReinicio.BackgroundImage = global::MarioKartCarrera.Properties.Resources.btnre;
+            this.btnReinicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReinicio.Location = new System.Drawing.Point(419, 92);
+            this.btnReinicio.Name = "btnReinicio";
+            this.btnReinicio.Size = new System.Drawing.Size(53, 52);
+            this.btnReinicio.TabIndex = 8;
+            this.btnReinicio.UseVisualStyleBackColor = false;
+            this.btnReinicio.Click += new System.EventHandler(this.btnReinicio_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.Color.Transparent;
+            this.btnDetener.BackgroundImage = global::MarioKartCarrera.Properties.Resources.btnquitar;
+            this.btnDetener.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDetener.Location = new System.Drawing.Point(507, 92);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(53, 52);
+            this.btnDetener.TabIndex = 9;
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = global::MarioKartCarrera.Properties.Resources.CircuitoMario_Kart;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnReinicio);
+            this.Controls.Add(this.btnPausa);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.C5);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C4);
+            this.Controls.Add(this.C2);
+            this.Location = new System.Drawing.Point(-20, 0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox C2;
+        private System.Windows.Forms.PictureBox C4;
+        private System.Windows.Forms.PictureBox C1;
+        private System.Windows.Forms.PictureBox C3;
+        private System.Windows.Forms.PictureBox C5;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnPausa;
+        private System.Windows.Forms.Button btnReinicio;
+        private System.Windows.Forms.Button btnDetener;
+    }
+}
+
---
+++ MarioKartCarrera/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarioKartCarrera
+{
+    public partial class Form1 : Form
+    {
+        Thread[] hilos;
+        int[] vueltasTotales;
+        PictureBox[] Personajes;
+        List<Point> recorrido;
+
+        public Form1()
+        {
+            // Corrección: evita problemas por escalado de DPI
+            this.AutoScaleMode = AutoScaleMode.None;
+
+            // Corrección: evita que el usuario cambie el tamaño de la ventana
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            InitializeComponent();
+
+            Personajes = new PictureBox[] { C1, C2, C3, C4, C5 };
+            hilos = new Thread[Personajes.Length];
+            vueltasTotales = new int[Personajes.Length];
+            InicializarRecorrido();
+        }
+
+        private void InicializarRecorrido()
+        {
+            recorrido = new List<Point>();
+
+            
+
+            for (int y = (int)(396 * 0.7); y >= (int)(81 * 0.7); y -= 11) 
+                recorrido.Add(new Point((int)(129 * 0.5556), y));
+            for (int x = (int)(129 * 0.5556); x <= (int)(580 * 0.5556); x += 8)
+                recorrido.Add(new Point(x, (int)(81 * 0.7))); 
+            for (int y = (int)(81 * 0.7); y >= (int)(127 * 0.7); y -= 11)
+                recorrido.Add(new Point((int)(580 * 0.5556), y));
+            for (int x = (int)(580 * 0.5556); x <= (int)(642 * 0.5556); x += 8) 
+                recorrido.Add(new Point(x, (int)(127 * 0.7))); 
+            for (int y = (int)(127 * 0.7); y <= (int)(196 * 0.7); y += 11)
+                recorrido.Add(new Point((int)(642 * 0.5556), y)); 
+            for (int x = (int)(642 * 0.5556); x <= (int)(697 * 0.5556); x += 8)
+                recorrido.Add(new Point(x, (int)(196 * 0.7))); 
+            for (int y = (int)(196 * 0.7); y <= (int)(268 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(697 * 0.5556), y)); 
+            for (int x = (int)(697 * 0.5556); x <= (int)(1184 * 0.5556); x += 8) 
+                recorrido.Add(new Point(x, (int)(268 * 0.7))); 
+            for (int y = (int)(268 * 0.7); y >= (int)(106 * 0.7); y -= 11) 
+                recorrido.Add(new Point((int)(1184 * 0.5556), y)); 
+            for (int x = (int)(1184 * 0.5556); x <= (int)(1255 * 0.5556); x += 8) 
+                recorrido.Add(new Point(x, (int)(106 * 0.7))); 
+            for (int y = (int)(106 * 0.7); y >= (int)(67 * 0.7); y -= 11) 
+                recorrido.Add(new Point((int)(1255 * 0.5556), y)); 
+            for (int x = (int)(1255 * 0.5556); x <= (int)(1628 * 0.5556); x += 8) 
+                recorrido.Add(new Point(x, (int)(67 * 0.7)));
+            for (int y = (int)(67 * 0.7); y <= (int)(105 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1628 * 0.5556), y)); 
+            for (int x = (int)(1628 * 0.5556); x <= (int)(1721 * 0.5556); x += 8) 
+                recorrido.Add(new Point(x, (int)(105 * 0.7))); 
+            for (int y = (int)(105 * 0.7); y <= (int)(366 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1721 * 0.5556), y)); 
+            for (int x = (int)(1721 * 0.5556); x >= (int)(1605 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(366 * 0.7))); 
+            for (int y = (int)(366 * 0.7); y <= (int)(409 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1605 * 0.5556), y)); 
+            for (int x = (int)(1605 * 0.5556); x >= (int)(1343 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(409 * 0.7))); 
+            for (int y = (int)(409 * 0.7); y <= (int)(442 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1343 * 0.5556), y)); 
+            for (int x = (int)(1343 * 0.5556); x >= (int)(1005 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(442 * 0.7)));
+            for (int y = (int)(442 * 0.7); y <= (int)(488 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1005 * 0.5556), y)); 
+            for (int x = (int)(1005 * 0.5556); x >= (int)(710 * 0.5556); x -= 8)
+                recorrido.Add(new Point(x, (int)(488 * 0.7))); 
+            for (int y = (int)(488 * 0.7); y <= (int)(615 * 0.7); y += 11)
+                recorrido.Add(new Point((int)(710 * 0.5556), y));
+            for (int x = (int)(710 * 0.5556); x <= (int)(1680 * 0.5556); x += 8) 
+                recorrido.Add(new Point(x, (int)(615 * 0.7))); 
+            for (int y = (int)(615 * 0.7); y <= (int)(676 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1680 * 0.5556), y));
+            for (int x = (int)(1680 * 0.5556); x <= (int)(1729 * 0.5556); x += 8) 
+                recorrido.Add(new Point(x, (int)(676 * 0.7))); 
+            for (int y = (int)(676 * 0.7); y <= (int)(840 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1729 * 0.5556), y)); 
+            for (int x = (int)(1729 * 0.5556); x >= (int)(1665 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(840 * 0.7))); 
+            for (int y = (int)(840 * 0.7); y <= (int)(877 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(1665 * 0.5556), y)); 
+            for (int x = (int)(1665 * 0.5556); x >= (int)(1124 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(877 * 0.7))); 
+            for (int y = (int)(877 * 0.7); y >= (int)(800 * 0.7); y -= 11) 
+                recorrido.Add(new Point((int)(1124 * 0.5556), y)); 
+            for (int x = (int)(1124 * 0.5556); x >= (int)(809 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(800 * 0.7))); 
+            for (int y = (int)(800 * 0.7); y >= (int)(762 * 0.7); y -= 11) 
+                recorrido.Add(new Point((int)(809 * 0.5556), y)); 
+            for (int x = (int)(809 * 0.5556); x >= (int)(666 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(762 * 0.7))); 
+            for (int y = (int)(762 * 0.7); y <= (int)(925 * 0.7); y += 11) 
+                recorrido.Add(new Point((int)(666 * 0.5556), y));
+            for (int x = (int)(666 * 0.5556); x >= (int)(199 * 0.5556); x -= 8) 
+                recorrido.Add(new Point(x, (int)(925 * 0.7))); 
+            for (int y = (int)(925 * 0.7); y >= (int)(823 * 0.7); y -= 11) 
+                recorrido.Add(new Point((int)(199 * 0.5556), y)); 
+            for (int x = (int)(199 * 0.5556); x >= (int)(144 * 0.5556); x -= 8)
+                recorrido.Add(new Point(x, (int)(823 * 0.7)));
+            for (int y = (int)(823 * 0.7); y >= (int)(396 * 0.7); y -= 11) 
+                recorrido.Add(new Point((int)(144 * 0.5556), y)); 
+        }
+
+        private void MoverCarro(PictureBox carro, int index, int vueltas)
+        {
+            Random localRand = new Random(Guid.NewGuid().GetHashCode());
+            int velocidadFija = localRand.Next(5,7);
+            int vueltasActuales = 0;
+
+            while (vueltasActuales < vueltas)
+            {
+                foreach (Point p in recorrido)
+                {
+                    carro.Invoke(new Action(() => carro.Location = p));
+                    Thread.Sleep(velocidadFija);
+                }
+                vueltasActuales++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Corrección: colocar todos los personajes al inicio del recorrido
+                for (int i = 0; i < Personajes.Length; i++)
+                {
+                    Personajes[i].Location = recorrido[0];
+                }
+
+                for (int i = 0; i < Personajes.Length; i++)
+                {
+                    int index = i;
+                    hilos[index] = new Thread(() => MoverCarro(Personajes[index], index, 3));
+                    hilos[index].Start();
+                }
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+
+        private void btnPausa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < hilos.Length; i++)
+                {
+                    if (hilos[i] != null && hilos[i].IsAlive)
+                        hilos[i].Suspend(); // ⚠️ Obsoleto
+                }
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+
+        private void btnReinicio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < hilos.Length; i++)
+                {
+                    if (hilos[i] != null && hilos[i].ThreadState == ThreadState.Suspended)
+                        hilos[i].Resume(); // ⚠️ Obsoleto
+                }
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < hilos.Length; i++)
+                {
+                    if (hilos[i] != null && hilos[i].IsAlive)
+                    {
+                        hilos[i].Abort(); // ⚠️ Obsoleto
+                        Personajes[i].Invoke(new Action(() => Personajes[i].Location = recorrido[0]));
+                    }
+                }
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+    }
+}
